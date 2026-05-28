ff421dbe84ca6ce56927728a33375012a7b96d67
Ariya Hidayat <ariya.hidayat@gmail.com>
2026-05-27 23:00:43 -0700

    Add mobile-friendly viewport meta tag

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.get('/', (c) =>
 <html lang="en">
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Sign In</title>
 </head>
 <body>
