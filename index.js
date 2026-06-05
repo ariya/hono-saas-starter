274e7a2074da077a70b1a8ff803464a1f3b06af9
Ariya Hidayat <ariya.hidayat@gmail.com>
2026-06-04 21:38:24 -0700

    feat: add randomized welcome title

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ const path = require('path');
 
 const eta = new Eta({ views: path.join(__dirname, 'views') });
 
+const WELCOME_TITLES = ['Welcome back', 'Good to see you again', 'Hello there', 'Welcome', 'Hey, welcome'];
+
 const app = new Hono();
 
 app.use(secureHeaders());
 
-app.get('/', (c) => c.html(eta.render('sign-in')));
+app.get('/', (c) => {
+  const title = WELCOME_TITLES[Math.floor(Math.random() * WELCOME_TITLES.length)];
+  return c.html(eta.render('sign-in', { title }));
+});
 
 app.get('/health', (c) => c.text(`OK ${Date.now()}`));
 
