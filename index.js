c7d08cd07ac4866d28b8d13df52dd139ccd6081c
Ariya Hidayat <ariya.hidayat@gmail.com>
2026-06-18 00:02:19 -0700

    Add responsive viewport meta tag to sign-in page

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.get('/', (c) => {
       '<html lang="en">' +
       '<head>' +
       '<meta charset="UTF-8">' +
+      '<meta name="viewport" content="width=device-width, initial-scale=1">' +
       '<title>Sign In</title>' +
       '</head>' +
       '<body>' +
