04632bc2f33c3fa2a45c72c9e326628862cc2ec9
Ariya Hidayat <ariya.hidayat@gmail.com>
2026-07-17 23:00:22 -0700

    Add responsive viewport meta tag

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.get('/', (c) => {
 <html lang="en">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sign in</title>
     <link rel="stylesheet" href="https://unpkg.com/@knadh/oat/oat.min.css">
   </head>
