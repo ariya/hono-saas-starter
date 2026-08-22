57bbc9ce51380b9e4d2e4379665d1c453ad5d3d3
Ariya Hidayat <ariya.hidayat@gmail.com>
2026-08-22 13:58:20 -0700

    Add responsive viewport and Oat styling

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const signInPage = `<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="https://unpkg.com/@knadh/oat/oat.min.css" />
+    <script src="https://unpkg.com/@knadh/oat/oat.min.js" defer></script>
     <title>Sign In</title>
   </head>
   <body>
