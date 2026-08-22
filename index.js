561827c3270d05014baf73e7c99f19c444410845
Ariya Hidayat <ariya.hidayat@gmail.com>
2026-08-22 14:40:23 -0700

    Randomize welcome title from five options

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const { Hono } = require('hono');
 const { serve } = require('@hono/node-server');
 const { secureHeaders } = require('hono/secure-headers');
 const { Eta } = require('eta');
+const { randomInt } = require('crypto');
 
 const eta = new Eta({ views: 'views' });
+
+const welcomeTitles = ['Welcome back', 'Hello again', 'Good to see you', 'Hey there', 'Greetings'];
 
 const app = new Hono();
 
 app.use(secureHeaders());
 
-app.get('/', (c) => c.html(eta.render('signin', {})));
+app.get('/', (c) => {
+  const title = welcomeTitles[randomInt(welcomeTitles.length)];
+  return c.html(eta.render('signin', { title }));
+});
 
 app.get('/health', (c) => c.text(`OK ${Date.now()}`));
 
